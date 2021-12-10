914d54d5193bd49833ce5cc7867e4866b32331f1
Jordan Bourbonnais <clonage@gmail.com>
2021-12-10 18:27:34 +0000

    first commit

---
+++ build/CMakeFiles/blender_project.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blender_project"
+  "CMakeFiles/blender_project-complete"
+  "blender_project-prefix/src/blender_project-stamp/blender_project-install"
+  "blender_project-prefix/src/blender_project-stamp/blender_project-mkdir"
+  "blender_project-prefix/src/blender_project-stamp/blender_project-download"
+  "blender_project-prefix/src/blender_project-stamp/blender_project-update"
+  "blender_project-prefix/src/blender_project-stamp/blender_project-patch"
+  "blender_project-prefix/src/blender_project-stamp/blender_project-configure"
+  "blender_project-prefix/src/blender_project-stamp/blender_project-build"
+  "blender_project-prefix/src/blender_project-stamp/blender_project-test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/blender_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/blender/CMakeFiles/blender.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blender.dir/blender.obj"
+  "blender.pdb"
+  "blender.wasm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blender.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
